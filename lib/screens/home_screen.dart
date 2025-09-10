b7998adb6fd8beb91f35e21fcde87e9bd9971a6e
mhmedhossam <momohossam11.mh@gmail.com>
2025-09-10 17:06:14 +0300

    edit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,53 +17,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<GoalsCubit, Goalstate>(
-      listener: (context, state) {
-        if (state is InsertDataSucceeded) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text("goal create success")));
-        }
-      },
-      builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 80,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [Colors.indigo[700]!, Colors.indigo[900]!],
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.indigo[700]!, Colors.indigo[900]!],
+            ),
+          ),
+        ),
+        centerTitle: true,
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "My Goals",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                fontSize: 30,
+                wordSpacing: 4,
               ),
             ),
-            centerTitle: true,
-            title: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "My Goals",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                    fontSize: 30,
-                    wordSpacing: 4,
-                  ),
-                ),
-                Text(
-                  "Track your progress",
-                  style: TextStyle(fontSize: 15, color: Colors.white70),
-                ),
-              ],
+            Text(
+              "Track your progress",
+              style: TextStyle(fontSize: 15, color: Colors.white70),
             ),
-          ),
-          body: SafeArea(
+          ],
+        ),
+      ),
+      body: BlocConsumer<GoalsCubit, Goalstate>(
+        listener: (context, state) {
+          if (state is InsertDataSucceeded) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text("goal create success")));
+          }
+        },
+        builder: (context, state) {
+          return SafeArea(
             child: Column(children: [MainContainer(), ListViewBuildGoals()]),
-          ),
-          floatingActionButton: FloatingActionButtonMainScreen(),
-        );
-      },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButtonMainScreen(),
     );
   }
 }
